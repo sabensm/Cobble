c79bb84344527e1763adde3ca6a2ccc2ed3a5c16
Mike Sabens <Michael.G.Sabens@espn.com>
2017-06-12 22:35:26 -0400

    Setup add image from photo library

--- Cobble/AddRecipeVC.swift
+++ Cobble/AddRecipeVC.swift
@@ -16,19 +16,36 @@ class AddRecipeVC: UIViewController, UINavigationControllerDelegate, UIImagePick
     @IBOutlet weak var recipeIngredients: UITextView!
     @IBOutlet weak var recipeInstructions: UITextView!
     @IBOutlet weak var recipeImage: UIImageView!
+    @IBOutlet weak var addImageButton: UIButton!
+    @IBOutlet weak var removeButton: UIButton!
+    
+    let imagePicker = UIImagePickerController()
     
     @IBAction func addRecipeImageButtonPressed(_ sender: Any) {
         
-        //Open image gallery to pick image
-        
-        //Set recipieImage to selected image.
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        imagePicker.modalPresentationStyle = .popover
+        present(imagePicker, animated: true, completion: nil)
+        imagePicker.popoverPresentationController?.sourceView = sender as? UIView
         
     }
+    
+    @IBAction func removeRecipeImageButtonPressed(_ sender: Any) {
+        
+        recipeImage.image = #imageLiteral(resourceName: "recipeImagePlaceholder")
+        removeButton.isHidden = true
+        addImageButton.isHidden = false
+        
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+
     }
     
     @IBAction func saveButtonPressed(_ sender: Any) {
@@ -40,6 +57,18 @@ class AddRecipeVC: UIViewController, UINavigationControllerDelegate, UIImagePick
         //dismiss View Controller
         
         _ = navigationController?.popViewController(animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        recipeImage.image = chosenImage
+        addImageButton.isHidden = true
+        removeButton.isHidden = false
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
     
 
